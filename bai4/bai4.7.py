16a5b315a33d6497be6bcc1b876677234c813ee7
anhlac123321-afk <anhlac123321@gmail.com>
2025-12-04 15:59:31 +0700

    Add files via upload

--- bai4/bai4.7.py
+++ bai4/bai4.7.py
@@ -1,9 +1,8 @@
-print("ho va ten:tran nhat anh")
-print("Mssv:245752021610069")
-chuoi = input("Nhập chuỗi: ")
-chuoi_moi = ""
-for ch in chuoi:
-    if not ch.isdigit():
-        chuoi_moi += ch
+chuoi = input('Nhap chuoi: ')
+ket_qua = ''                       
 
-print("Chuỗi sau khi loại bỏ chữ số:", chuoi_moi)
+for ch in chuoi:
+    if not ch.isdigit():          
+        ket_qua += ch             
+
+print('Chuoi sau khi loai bo chu so:', ket_qua)
